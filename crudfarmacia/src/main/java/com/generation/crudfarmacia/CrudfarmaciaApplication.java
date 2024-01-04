db018bf2bc40e0e0aeca47dd66a850bb5c6a3315
Joao Pedro Oliveira Rochaq <pedrorocha2info@gmail.com>
2024-01-04 12:25:50 -0300

    irst

---
+++ crudfarmacia/src/main/java/com/generation/crudfarmacia/CrudfarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.crudfarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudfarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudfarmaciaApplication.class, args);
+	}
+
+}
---
+++ crudfarmacia/src/test/java/com/generation/crudfarmacia/CrudfarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.crudfarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudfarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
